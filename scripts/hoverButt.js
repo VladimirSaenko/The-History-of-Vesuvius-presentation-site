fe9ab6bc8f0409f1934d89a1a2a24a76c421b68c
Vladimir Saenko <vova.saenko1@gmail.com>
2021-03-27 18:17:07 +0200

    add site

---
+++ scripts/hoverButt.js
@@ -0,0 +1,15 @@
+
+"use strict"
+
+let button = document.getElementById("butt");
+let text = document.getElementById("text");
+
+button.addEventListener('mousemove',function() {
+    button.style.background = "black";
+    text.style.color = "white";
+})
+
+button.addEventListener('mouseout', function() {
+    button.style.background = "white";
+    text.style.color = "black";
+})
